0419956d1c3398712bc4ad2c245a6dbb75d01ea8
Lunkn <Phelsuma_66@hotmail.com>
2017-04-26 14:35:25 +0200

    Funktion HM

---
+++ HalveringMetod.m
@@ -0,0 +1,21 @@
+function f = HalveringMetod(e)
+    skar = @(t) (2*t*sin(2*t - 1.5))*((2*t*sin(2*t - 1.5)) + (4*t*cos(t)+1.5)) + 105*t.^2 - 920;
+    a = -2*pi;
+    b = 2*pi;
+    ap = a;
+    bp = b;
+    
+    for i=0:5
+        if(ap - bp < e)
+            break;
+        end
+        m = ((ap + bp)/2);
+        if(sign(skar(ap)skar(m)) < 0)
+            bp = m;
+        end
+        if(sign(fxy(bp)fxy(m)) < 0)
+            ap = m;
+        end
+    end
+    f = (ap:bp);
+end
