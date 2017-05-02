66055ae11ee17e37e48b372f90df4fdf8c201b06
Johan Almeflo <johan_almeflo@hotmail.com>
2017-05-02 15:53:43 +0200

    Fix x values in SimpsonsRule

--- SimpsonsRule.m
+++ SimpsonsRule.m
@@ -5,10 +5,19 @@ function f = SimpsonsRule(a, b)
   
   sum = 0;
   
-  for k = 1:n      
-    f1 = abs(norm(v_prim(2*k - 2, h_prim)));
-    f2 = abs(norm(v_prim(2*k - 1, h_prim)));
-    f3 = abs(norm(v_prim(2*k, h_prim)));
+  for k = 1:n
+    k1 = 2*k - 2;
+    k2 = 2*k - 1;
+    k3 = 2*k;
+    
+    x1 = a + k1*h;
+    x2 = a + k2*h;
+    x3 = a + k3*h;
+    
+    f1 = abs(norm(v_prim(x1, h_prim)));
+    f2 = abs(norm(v_prim(x2, h_prim)));
+    f3 = abs(norm(v_prim(x3, h_prim)));
+    
     sum = sum + (f1 + 4*f2 + f3);    
   end
   sum = (h / 3) * sum;
