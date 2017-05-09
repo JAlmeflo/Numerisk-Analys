f7750fc15e089e9ccd050c851c3afb83def44956
Johan Almeflo <johan_almeflo@hotmail.com>
2017-05-09 14:23:53 +0200

    Fix comments in CubicSpline

--- CubicSpline.m
+++ CubicSpline.m
@@ -1,26 +1,13 @@
 function f = CubicSpline(x, y)
   sz = size(x, 2);
   
-  % Si(x) = ai + bi*ui + ci*ui^2 + di*ui^3;
-  
-  % ui = (x - x(i-1)) / hi
-  
-  % ai = y(i-1)
-  % bi = hi*k(i-1)
-  % ci = 3(yi - y(i-1)) - hi(2k(i-1) + ki)
-  % di = 2(y(i-1) - yi) + hi(k(i-1) + ki)
-  
-  % hi = xi - x(i-1)
-  % ki = bi/hi + 2ci/hi + 3di/hi
-  
-  % Calculate h
+  % h
   h = zeros(1, sz);
   for i = 2:sz
     h(i) = x(i) - x(i - 1);
   end
   
-  % Calculate k
-  % Ak = r
+  % Init values
   A = zeros(sz);
   k = zeros(sz, 1);
   r = zeros(sz, 1);
@@ -44,24 +31,17 @@ function f = CubicSpline(x, y)
   end
   
   % k
-  % Todo: Calculate k from Ak = r;
   k = A\r;
   
   % Calculate spline S
-  %S = zeros(1,sz);
   S{1} = @(xx) 0;
   for i = 2:sz % Start from 2 because we need x(i-1)
-    % kn_1 + 2kn = rn
-    % rn = 3*((yn - yn_1) / hn);
-    %ki = 0; % Todo: what is this even?
-    %ki_1 = 0; % Todo: and this is what?
     
     ai = y(i - 1);
     bi = h(i) * k(i-1);
     ci = 3*(y(i) - y(i - 1)) - h(i)*(2*k(i-1) + k(i));
     di = 2*(y(i - 1) - y(i)) + h(i)*(k(i-1) + k(i));
     
-    % ui = (x - x(i - 1)) / hi;    
     S{i} = @(xx) ai + bi*((xx - x(i - 1)) / h(i)) + ci*((xx - x(i - 1)) / h(i)).^2 + di*((xx - x(i - 1)) / h(i)).^3;
   end
   f = S;
